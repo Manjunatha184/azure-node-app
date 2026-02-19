89503672aeb2763127362e9fb44e41560462e420
Manjunatha184 <shankarmanjunath184@gmail.com>
2026-02-19 16:41:43 +0530

    Updated message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Azure App app Service 🚀");
+  res.send("Deployed automatically from GitHub 🚀🔥");
 });
 
 app.listen(port, () => {
